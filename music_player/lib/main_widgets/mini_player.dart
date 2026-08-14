d2a23a6048d07837a023abcee82666a3687df4a6
ap0100 <pecevska9@gmail.com>
2026-08-14 21:50:59 +0200

    mini player style 1.0

--- music_player/lib/main_widgets/mini_player.dart
+++ music_player/lib/main_widgets/mini_player.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import '../theme/jinx_style.dart';
 
 class MiniPlayer extends StatelessWidget {
   final Video? video;
@@ -25,129 +26,133 @@ class MiniPlayer extends StatelessWidget {
   Widget build(BuildContext context) {
     if (video == null) return SizedBox.shrink();
 
+    var borderColor = searchFocused
+        ? JinxTheme.deepCerise
+        : artistProfileFocused
+        ? JinxTheme
+              .tyrianPurple //Color.fromARGB(255, 124, 78, 120)
+        : JinxTheme.brightTurquoise;
+
     return GestureDetector(
       onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.only(right: 1),
-        height: 65,
-        decoration: BoxDecoration(
-          border: Border(
-            top: BorderSide(
-              color: searchFocused
-                  ? const Color.fromARGB(255, 216, 71, 221)
-                  : artistProfileFocused
-                  ? Color.fromARGB(255, 124, 78, 120)
-                  : const Color.fromARGB(255, 40, 163, 163),
-              width: 1,
+      child: Stack(
+        children: [
+          ClipPath(
+            clipper: MiniPlayerClipper(offset: 1.5),
+            child: Container(
+              height: 70,
+              color: borderColor.withValues(alpha: 0.9),
             ),
           ),
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(5),
-            topRight: Radius.circular(5),
+          ClipPath(
+            clipper: MiniPlayerClipper(),
+            child: Transform.translate(
+              offset: Offset(0, 5),
+              child: Container(
+                padding: EdgeInsets.only(right: 1.2),
+                height: 70,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      (JinxTheme.darkCold).withValues(
+                        alpha: 0.95,
+                      ), //Color.fromARGB(255, 15, 20, 20),
+                      (JinxTheme.darkWarm).withValues(
+                        alpha: 0.95,
+                      ), //Color.fromARGB(255, 34, 28, 46),
+                    ],
+                    stops: [0, 1.5],
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Stack(
+                      children: [
+                        ClipPath(
+                          clipper: MiniPlayerClipper(cover: true, offset: 0.9),
+                          child: Container(
+                            width: 106,
+                            height: 60,
+                            color: borderColor.withValues(alpha: 0.9),
+                          ),
+                        ),
+                        ClipPath(
+                          clipper: MiniPlayerClipper(cover: true),
+                          child: SizedBox(
+                            width: 106,
+                            height: 60,
+                            //margin: const EdgeInsets.only(left: 4, right: 10),
+                            child: CachedNetworkImage(
+                              imageUrl:
+                                  video?.thumbnails.highResUrl ??
+                                  video?.thumbnails.standardResUrl ??
+                                  '',
+                              fit: BoxFit.cover,
+                              width: 90,
+                              height: 100,
+                              errorWidget: (context, url, error) => Image.asset(
+                                'lib/graphics/no_thumbnail_found.jpg',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            //),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 17),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(right: 60),
+                        child: Text(
+                          video!.title,
+                          style: TextStyle(
+                            color: JinxTheme
+                                .violetBlue, // const Color.fromARGB(255, 205, 248, 241),
+                            fontSize: 14,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      icon: StreamBuilder<bool>(
+                        stream: player.playerStateStream
+                            .map((state) => state.playing)
+                            .distinct(),
+                        initialData: player.playing,
+                        builder: (context, snapshot) {
+                          final isPlaying = snapshot.data ?? false;
+                          return Icon(
+                            isPlaying
+                                ? Icons.pause_circle_filled_sharp
+                                : Icons.play_circle_outline_sharp,
+                            size: 37,
+                            color: borderColor,
+                          );
+                        },
+                      ),
+                      onPressed: () {
+                        player.playing ? player.pause() : player.play();
+                      },
+                    ),
+                    IconButton(
+                      onPressed: onTap,
+                      icon: Icon(
+                        Icons.arrow_upward,
+                        color: JinxTheme
+                            .violetBlue, //Color.fromARGB(255, 227, 156, 236),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          gradient: LinearGradient(
-            begin: const Alignment(0.0, 0.2),
-            end: const Alignment(0.0, 1.6),
-            colors: const [
-              Color.fromARGB(255, 15, 20, 20),
-              Color.fromARGB(255, 34, 28, 46),
-            ],
-          ),
-        ),
-        child: Row(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 0, bottom: 0, left: 1, right: 0),
-              width: 90,
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(5)),
-                border: Border(
-                  right: BorderSide(
-                    color: searchFocused
-                        ? const Color.fromARGB(255, 216, 71, 221)
-                        : artistProfileFocused
-                        ? Color.fromARGB(255, 124, 78, 120)
-                        : const Color.fromARGB(255, 40, 163, 163),
-                    width: 1,
-                  ),
-                ),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(5),
-                  bottomLeft: Radius.circular(5),
-                ),
-                child: CachedNetworkImage(
-                  imageUrl:
-                      video?.thumbnails.highResUrl ??
-                      video?.thumbnails.standardResUrl ??
-                      '',
-                  fit: BoxFit.cover,
-                  width: 90,
-                  height: 100,
-                  errorWidget: (context, url, error) => Image.asset(
-                    'lib/graphics/no_thumbnail_found.jpg',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(width: 17),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(right: 60),
-                child: Text(
-                  video!.title,
-                  style: TextStyle(
-                    color: Color.fromARGB(
-                      255,
-                      229,
-                      185,
-                      235,
-                    ), // const Color.fromARGB(255, 205, 248, 241),
-                    fontSize: 14,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-            ),
-            IconButton(
-              icon: StreamBuilder<bool>(
-                stream: player.playerStateStream
-                    .map((state) => state.playing)
-                    .distinct(),
-                initialData: player.playing,
-                builder: (context, snapshot) {
-                  final isPlaying = snapshot.data ?? false;
-                  return Icon(
-                    isPlaying
-                        ? Icons.pause_circle_filled_sharp
-                        : Icons.play_circle_outline_sharp,
-                    size: 37,
-                    color: searchFocused
-                        ? const Color.fromARGB(255, 216, 71, 221)
-                        : artistProfileFocused
-                        ? Color.fromARGB(255, 124, 78, 120)
-                        : const Color.fromARGB(255, 40, 163, 163),
-                  );
-                },
-              ),
-              onPressed: () {
-                player.playing ? player.pause() : player.play();
-              },
-            ),
-            IconButton(
-              onPressed: onTap,
-              icon: Icon(
-                Icons.arrow_upward,
-                color: Color.fromARGB(255, 227, 156, 236),
-              ),
-            ),
-          ],
-        ),
+        ],
       ),
     );
   }
--- music_player/lib/theme/jinx_style.dart
+++ music_player/lib/theme/jinx_style.dart
@@ -1,36 +1,53 @@
 import 'package:flutter/material.dart';
 
 class JinxTheme {
-  static const Color deepCerise = Color(0xFFFD3EFD);
-  static const Color brightTurquoise = Color(0xFF22EFF2);
-  static const Color tyrianPurple = Color.fromARGB(255, 85, 5, 72);
+  static const Color deepCerise = Color.fromARGB(169, 253, 62, 253);
+  static const Color brightTurquoise = Color.fromARGB(148, 34, 239, 242);
+  static const Color tyrianPurple = Color.fromARGB(255, 131, 7, 110);
   static const Color regalBlue = Color.fromRGBO(30, 57, 87, 1);
   static const Color darkWarm = Color.fromARGB(255, 36, 8, 16);
   static const Color darkCold = Color.fromARGB(255, 8, 36, 32);
-  static const Color violetBlue = Color(0xFFB0569D);
+  static const Color violetBlue = Color.fromARGB(255, 235, 97, 205);
   static const Color midnightExpress = Color(0xFF0E1C3F);
 }
 
-class BeveledClipper extends CustomClipper<Path> {
-  final double bevelSize;
+class MiniPlayerClipper extends CustomClipper<Path> {
+  final double offset;
+  final bool cover;
 
-  BeveledClipper({this.bevelSize = 10});
+  MiniPlayerClipper({this.offset = 0, this.cover = false});
 
   @override
   Path getClip(Size size) {
     final path = Path();
     final w = size.width;
     final h = size.height;
-    final b = bevelSize;
 
-    path.moveTo(b, 0);
-    path.lineTo(w - b, 0);
-    path.lineTo(w, b);
-    path.lineTo(w, h - b);
-    path.lineTo(w - b, h);
-    path.lineTo(b, h);
-    path.lineTo(0, h - b);
-    path.lineTo(0, b);
+    if (cover) {
+      path.lineTo(0, h);
+      path.lineTo(0, (h - 45));
+      path.lineTo(15, (h - 53));
+      path.lineTo(w + offset, (h - 60));
+      path.lineTo((w - 10) + offset, h - 50);
+      path.lineTo((w - 15) + offset, h - 40);
+      path.lineTo((w - 10) + offset, h - 30);
+      path.lineTo((w - 12) + offset, h - 10);
+      path.lineTo((w - 5) + offset, h);
+      path.lineTo(0, h);
+      path.close();
+
+      return path;
+    }
+
+    path.lineTo(0, h);
+    path.lineTo(0, (h - 45) - offset);
+    path.lineTo(15, (h - 55) - offset);
+    path.lineTo(100, (h - 53) - offset);
+    path.lineTo(110, (h - 60) - offset);
+    path.lineTo(w - 15, (h - 60) - offset);
+    path.lineTo(w, (h - 53) - offset);
+    path.lineTo(w, h);
+    path.lineTo(0, h);
     path.close();
 
     return path;
@@ -38,40 +55,4 @@ class BeveledClipper extends CustomClipper<Path> {
 
   @override
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
-}
-
-class BeveledContainer extends StatelessWidget {
-  final Widget child;
-  final double bevelSize;
-  final Color? color;
-  final Color? borderColor;
-  final double borderWidth;
-  final EdgeInsets padding;
-
-  const BeveledContainer({
-    required this.child,
-    this.bevelSize = 12.0,
-    this.color = JinxTheme.regalBlue,
-    this.borderColor,
-    this.borderWidth = 1.5,
-    this.padding = const EdgeInsets.all(12),
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipPath(
-      clipper: BeveledClipper(bevelSize: bevelSize),
-      child: Container(
-        padding: padding,
-        decoration: BoxDecoration(
-          color: color,
-          border: borderColor != null
-              ? Border.all(color: borderColor!, width: borderWidth)
-              : null,
-        ),
-        child: child,
-      ),
-    );
-  }
 }
